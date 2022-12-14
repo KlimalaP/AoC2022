e9e3e346aae8070f6901de5343debbc7a585ba3b
Piotr <piotrklimala1@gmail.com>
2022-12-14 22:17:43 +0100

    Day 3 part 1 of AoC solved puzzle

---
+++ Day3/Day3.py
@@ -0,0 +1,31 @@
+import re
+
+arr = []
+score = 0
+alphabet_higher_case = {chr(i+64): i + 26 for i in range(1, 27)}
+alphabet_lower_case = {chr(i+96): i for i in range(1, 27)}
+
+with open('input.txt', 'r') as f:
+    for line in f:
+        lhs = line[:len(line)//2]
+        rhs = line[len(line)//2:]
+        for char in lhs:
+            pattern = re.compile(char)
+            match = pattern.search(rhs)
+            if match:
+                arr.append(match.group())
+                break
+
+for i in arr:
+    if i.islower():
+        try:
+            score = score + alphabet_lower_case.get(i)
+        except TypeError:
+            print("NoneType")
+    else:
+        try:
+            score = score + alphabet_higher_case.get(i)
+        except TypeError:
+            print("NoneType")
+
+print(score)
